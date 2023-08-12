e76804c9fe8349f0b59e659fbed0cb50e57022b9
kristiansnts <epafroditus.kritian@gmail.com>
2023-08-12 08:33:48 +0700

    add feature sketchSize

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
 const container = document.querySelector('.container');
-let boxs = '';
 
-function makeBox(n){
+let sketchSize = document.querySelector('input[type="range"]');
+sketchSize.addEventListener('change', function(e){
+    let n = 0;
+    n = parseInt(e.target.value)
+    let result = makeBox(n);
+    container.innerHTML = result;
+})
+
+function makeBox(n = 1){
+    let boxs = '';
     for(let i = 1; i <= n**2; i++){
         boxs += `
             <div 
                 class="box" 
                 style="width: ${100 / n}%; 
-                border: ${(n < 2) ? '0px' : '1px' } solid salmon;
+                border: ${(n < 2) ? '0px' : '1px' } solid rgba(0,0,0,0.1);
             ">
-
             </div>
         `;
     }
     return boxs;    
 }
 
-// let color = prompt('get color');
-
-function sketch(box, color){
+function sketch(box, colorVal){
+    const color = colorVal;
     box.style.backgroundColor = color;
 }
 
-container.addEventListener('mousemove', (e) => {
-    if(e.target.classList.contains('box')){
-        const box = e.target;
-        sketch(box, color);
-    }
-})
-
-
-
-const result = makeBox(9);
-
-container.innerHTML = result;
+const colorPicker = document.querySelector('input[type="color"]');
+colorPicker.addEventListener("change", function(e) {
+    const colorVal = e.target.value;
+    container.addEventListener('mousemove', (e) => {
+        if(e.target.classList.contains('box')){
+            const box = e.target;
+            sketch(box, colorVal);
+        }
+    })
+});
